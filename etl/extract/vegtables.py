bfffb02e68feac809cb3cd57be1466d5f06e644d
senhuang2024 <senhuang@hotmail.co.uk>
2025-01-24 01:42:59 +0000

    final 8

--- etl/extract/vegtables.py
+++ etl/extract/vegtables.py
@@ -9,7 +9,7 @@ def get_veg_data():
     
     # traverse up to the project root folder
     print(os.path.dirname(__file__))
-    root_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../../../'))
+    root_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
     root_folder = os.path.join(root_folder,"etl/data")
     
     file_names = [f for f in os.listdir(root_folder) if os.path.isfile(os.path.join(root_folder, f))]
